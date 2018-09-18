ee8b2e78ce2fa26ad592fa535efb1a76fffba163
c0ncon <c4rneade@gmail.com>
2018-09-18 15:58:51 +0900

    dcinside 2018-09 update(WIP)

--- dcv.js
+++ dcv.js
@@ -2,8 +2,8 @@
 // @name     dcv
 // @version  1
 // @grant    none
-// @match    http://gall.dcinside.com/board/lists/*
-// @match    http://gall.dcinside.com/mgallery/board/lists/*
+// @match    http://gall.dcinside.com/board/lists*
+// @match    http://gall.dcinside.com/mgallery/board/lists*
 // ==/UserScript==
 (function (window) {
   let config = JSON.parse(localStorage.getItem('dcv')) || {
@@ -32,7 +32,7 @@
   };
 
   const init = () => {
-    let galleryList = document.querySelector('.gallery_list');
+    let galleryList = document.querySelector('.gall_list').cloneNode(true);
     galleryList.style.padding = '0';
     galleryTitle = document.title;
     let body = document.body;
@@ -41,9 +41,51 @@
       height: '100%'
     });
 
+    let galleryPaging = document.querySelector('div.bottom_paging_box').cloneNode(true);
+    let gallerySearching = document.querySelector('form[name=frmSearch]').cloneNode(true);
+    Object.assign(gallerySearching.style, {
+      marginBottom: '50px'
+    });
+
+    let galleryTop = document.createElement('div');
+    galleryTop.id = 'galleryTop';
+    Object.assign(galleryTop.style, {
+      height: '50px',
+      position: 'fixed',
+      backgroundColor: '#fff',
+      top: 0,
+      left: 0,
+      width: '100%',
+      zIndex: 1
+    });
+    let hiddenForms = document.querySelector('form[name=frm]')
+    galleryTop.appendChild(hiddenForms.cloneNode(true));
+
+    let galleryButtonDiv = document.createElement('div');
+    Object.assign(galleryButtonDiv.style, {
+      float: 'left',
+      position: 'absolute',
+      top: '50%',
+      transform: 'translate(0, -50%)'
+    });
+
+    document.querySelectorAll('div.list_bottom_btnbox button').forEach((button) => {
+      let btn = button.cloneNode(true);
+      btn.style.margin = '5px';
+      galleryButtonDiv.appendChild(btn);
+    });
+    let outputArray = document.querySelector('div.output_array');
+    galleryButtonDiv.appendChild(outputArray.cloneNode(true));
+    galleryTop.appendChild(galleryButtonDiv);
+
+    let favoritesDiv = getFavoritesDiv();
+    galleryTop.appendChild(favoritesDiv);
+
     let scripts = [
-      "http://gall.dcinside.com/_js/ZeroClipboard.min.js",
-      "http://gall.dcinside.com/_js/vr_clipboard.min.js"
+      // "http://gall.dcinside.com/_js/ZeroClipboard.min.js",
+      // "http://gall.dcinside.com/_js/vr_clipboard.min.js",
+      "http://gall.dcinside.com/_js/dc_common.js",
+      "http://gall.dcinside.com/_js/comment.js"
     ];
     scripts.forEach((script) => {
       let s = document.createElement('script');
@@ -71,67 +113,41 @@
     Object.assign(modalInner.style, {
       backgroundColor: '#fff',
       borderRadius: '3px',
-      maxWidth: '1100px',
+      maxWidth: '1160px',
       margin: '25px auto',
       padding: '25px',
       overflow: 'hidden'
     });
-
     modalDiv.appendChild(modalInner);
 
-    let galleryTop = document.createElement('div');
-    galleryTop.id = 'galleryTop';
-    Object.assign(galleryTop.style, {
-      height: '50px',
-      position: 'fixed',
-      backgroundColor: '#fff',
-      top: 0,
-      left: 0,
-      width: '100%',
-      zIndex: 1
-    });
-    let galleryButtonDiv = document.createElement('div');
-    Object.assign(galleryButtonDiv.style, {
-      float: 'left',
-      position: 'absolute',
-      top: '50%',
-      transform: 'translate(0, -50%)'
-    });
-    galleryList.querySelectorAll('.btn_bottom > span > a').forEach((button) => {
-      let btn = button.cloneNode(true);
-      btn.querySelector('img').style.margin = '5px';
-      galleryButtonDiv.appendChild(btn);
-    });
-    galleryTop.appendChild(galleryButtonDiv);
-
-    let favoritesDiv = getFavoritesDiv();
-    galleryTop.appendChild(favoritesDiv);
-
-    body.appendChild(galleryTop);
-
-    let galleryLeft = document.createElement('div');
-    galleryLeft.id = 'dgn_gallery_left';
-    Object.assign(galleryLeft.style, {
+    let listContainer = document.createElement('div');
+    listContainer.className = 'list';
+    Object.assign(listContainer.style, {
       position: 'relative',
       overflow: 'auto',
       margin: '50px auto',
       width: '100%',
       height: '100%'
     });
-    galleryLeft.appendChild(galleryList);
-    body.appendChild(galleryLeft);
+    listContainer.appendChild(galleryList);
+
+    body.appendChild(galleryTop);
+    body.appendChild(listContainer);
+    body.appendChild(galleryPaging);
+    body.appendChild(gallerySearching);
     body.appendChild(modalDiv);
 
-    document.querySelectorAll('table > tbody > tr > td.t_subject > a').forEach((el) => {
-      if (el.className) {
+    document.querySelectorAll('table > tbody > tr > td.gall_tit > a').forEach((el) => {
+      if (!el.className) {
         el.removeEventListener('click', openContentModal);
         el.addEventListener('click', openContentModal);
       }
     });
 
-    document.addEventListener('keyup', (event) => {
-      let modal = document.querySelector('#contentModal');
-      if (event.keyCode === 27 && modal) {
+    document.addEventListener('keydown', (event) => {
+      // 27 ESC, 8 Backspace
+      if (event.keyCode === 27 || event.keyCode === 8) {
+        event.preventDefault();
         closeContentModal();
       }
     });
@@ -222,55 +238,62 @@
       let template = document.createElement('template');
       template.innerHTML = responseText.trim();
       document.title = template.content.querySelector('title').innerHTML;
+      
+      template.content.querySelector('form[name=frm]').childNodes.forEach((articleHiddenForm) => {
+        if (!articleHiddenForm.id) return;
+
+        let t = articleHiddenForm.cloneNode(true);
+        let found = false;
+        let listHiddenForms = document.querySelector('form[name=frm]');
+        listHiddenForms.childNodes.forEach((form) => {
+          if (form.id === t.id) {
+            found = true;
+            listHiddenForms.replaceChild(t, form);
+          };
+        });
+        if (!found) {
+          listHiddenForms.appendChild(t);
+        };
+      })
 
       let content = document.createElement('div');
-      content.id = 'dgn_content_de';
-      content.appendChild(template.content.querySelector('.re_gall_top_1'));
-      content.appendChild(template.content.querySelector('.re_gall_box_1'));
-      // content.appendChild(template.content.querySelector('.re_gall_box_2'));
-      template.content.querySelector('.re_gall_box_3')
-        ? content.appendChild(template.content.querySelector('.re_gall_box_3'))
-        : content.appendChild(document.createElement('div'));
+      content.className = 'view_content_wrap';
 
-      let replyArea = template.content.querySelector('.re_gall_box_4');
-      replyArea.querySelector('.gallery_re_form').remove();
-      content.appendChild(replyArea);
+      content.appendChild(template.content.querySelector('div.gallview_head').cloneNode(true));
+      let body = template.content.querySelector('div.gallview_contents').cloneNode(true);
+      body.childNodes.forEach((node) => {
+        if (node.className === 'sch_alliance_box' || node.className === 'power_click') node.remove();
+      });
+      content.appendChild(body);
+      let commentContainer = template.content.querySelector('div.view_comment').cloneNode(true);
+      commentContainer.querySelector('.cmt_write_box').remove();
+      content.appendChild(commentContainer);
 
-      let replyPerPage = document.createElement('input');
-      replyPerPage.id = 'comment_num';
-      replyPerPage.value = '100';
-      replyPerPage.type = 'hidden';
-      content.appendChild(replyPerPage);
-
-      let replyCount = content.querySelector('#re_count').innerHTML;
       let scr = document.createElement('script');
       let scrString = `
-        comment_list(${replyCount});
-        var Pager = {
-          pageIndexChanged: function (selectedPage) {
-            _currentPage = selectedPage;
-            comment_page(${replyCount});
-          }
-        }
+        viewComments(1, 'VIEW_PAGE');
       `;
       scr.innerHTML = scrString;
       content.appendChild(scr);
 
       modal.appendChild(document.importNode(content, true));
-    }).catch((e) => console.log(e));
+    }).catch((e) => {
+      console.log(e);
+      closeContentModal();
+    });
   };
 
   const closeContentModal = () => {
-    Object.assign(document.body.style, {
-      margin: '',
-      overflow: ''
-    });
-    document.querySelector('#contentModal').style.display = 'none';
-
-    let modalBody = document.querySelector('#modalBody');
-    modalBody.removeChild(modalBody.firstChild);
-    history.back();
-    document.title = galleryTitle;
+    if (document.querySelector('#contentModal').style.display === 'block') {
+      Object.assign(document.body.style, {
+        margin: '',
+        overflow: ''
+      });
+      document.querySelector('#contentModal').style.display = 'none';
+      document.querySelector('#modalBody').firstChild.remove();
+      history.back();
+      document.title = galleryTitle;
+    }
   };
 
   init();
